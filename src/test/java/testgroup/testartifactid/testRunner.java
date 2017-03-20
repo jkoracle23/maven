b425df6015bfc48c3c7528b3d30be6ae518e7b6d
jitendra <jitendrakumar.mathmagic@gmail.com>
2017-03-20 06:52:47 +0530

    merge conflict changes

--- src/test/java/testgroup/testartifactid/testRunner.java
+++ src/test/java/testgroup/testartifactid/testRunner.java
@@ -9,9 +9,12 @@ public class testRunner {
 	@Test
 	public  void Test()
 	{
+		System.out.println("merge conflict changes");
 		System.out.println("hi");
 		
 		Assert.assertEquals("this is message", "test", "not test");
+		System.out.println("another merge conflict changes");
+		
 	}
 	
 	
